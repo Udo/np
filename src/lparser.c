d8efa4e4d392b721520aba3a93b0df84095e54ed
Udo Schroeter <udo.schroeter@gmail.com>
2014-07-28 05:02:48 +0200

    fixed if statement error that swallowed up the else part

--- src/lparser.c
+++ src/lparser.c
@@ -1646,12 +1646,22 @@ static void test_then_block (LexState *ls, int *escapelist) {
 static void ifstat (LexState *ls, int line) {
   /* ifstat -> IF cond THEN block {ELSEIF cond THEN block} [ELSE block] END */
   FuncState *fs = ls->fs;
+	BlockCnt bl;
   int escapelist = NO_JUMP;  /* exit list for finished parts */
   test_then_block(ls, &escapelist);  /* IF cond THEN block */
   while (ls->t.token == TK_ELSEIF)
     test_then_block(ls, &escapelist);  /* ELSEIF cond THEN block */
-  if (testnext(ls, TK_ELSE))
-    block(ls);  /* `else' part */
+  if (testnext(ls, TK_ELSE)) {
+  	int doFound = testnext(ls, TK_DO);
+		enterblock(fs, &bl, 0);
+	  if(doFound == 0)
+	    statement(ls);
+	  else {
+	    statlist(ls);  
+			testnext(ls, TK_END);
+	  }
+	  leaveblock(fs);
+  }
   //check_match(ls, TK_END, TK_IF, line);
   luaK_patchtohere(fs, escapelist);  /* patch escape list to 'if' end */
 }
