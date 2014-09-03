7400015611b26dc00db6f298eea2e6e367fac9a1
Udo Schroeter <udo.schroeter@gmail.com>
2014-09-03 14:31:59 +0200

    cgidemo

--- site/invoke/cgidemo.php
+++ site/invoke/cgidemo.php
@@ -30,14 +30,32 @@ function cmd_exec($cmd, &$stdout, &$stderr)
 }
 
 ?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>np</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=1024">
+        <base href="http://np-lang.org/">
+        <link rel="stylesheet" href="/css/">
+        <link rel="icon" type="image/png" href="favicon.png">
+        <script src="/js/"></script>
+    </head>
+    <body><article style="text-align: left;">
+<form method="post" action="/invoke/cgidemo.php">
 
-<form method="post" action="cgidemo.php">
-
-  <textarea name="code" id="code" placeholder="Your np code here"><?= htmlspecialchars($_REQUEST['code']) ?></textarea>
+  <textarea class="pre" name="code" id="code" 
+    autocapitalize="off" autocorrect="off" autocomplete="off"  spellcheck="false"
+    placeholder="Your np code here"><?= htmlspecialchars($_REQUEST['code']) ?></textarea>
   <input type="submit" value="⇲ Run"/>
-
-<h3>Output:</h3>
 </form>
+<h3>Output:</h3>
+
 
 <?php
 
@@ -49,7 +67,7 @@ root.sys = nil
 root.lib = nil
 root.io = nil
 root.os = nil
-root.require = nil
+
 '.file_get_contents('cgidemo-examplestate.np').chr(10).$_REQUEST['code']);
   $err = '';
   cmd_exec('timeout --preserve-status 0.01s /srv/www/htdocs/openfu.com/dev/np/bin/np '.$tmpFile, $out, $err);
@@ -59,42 +77,45 @@ root.require = nil
   unlink($tmpFile);
 }
 
+$b1 = '#fcfcfc';
+$b2 = '#f8f8f8';
+
 ?>
 
 <pre id="out"><?= htmlspecialchars($out) ?></pre>
 
 <style>
-  form {
 
+  pre {
+    margin-top:-12px;
+    padding:0;
   }
 
-  #out {
-    width: 100%;
-  }
-
-  * {
-    font-family: Tahoma, Helvetica;
+  textarea, input[type=submit] {
+    border: 1px solid rgba(0,0,0,0.1);
+    background: rgba(0,0,50,0.03);
   }
 
   textarea {
-    border: 1px solid #369;
-    width: 99%;
+    background: -webkit-linear-gradient(<?= $b1 ?>, <?= $b2 ?>); /* For Safari 5.1 to 6.0 */
+    background: -o-linear-gradient(<?= $b1 ?>, <?= $b2 ?>); /* For Opera 11.1 to 12.0 */
+    background: -moz-linear-gradient(<?= $b1 ?>, <?= $b2 ?>); /* For Firefox 3.6 to 15 */
+    background: linear-gradient(<?= $b1 ?>, <?= $b2 ?>); /* Standard syntax */
+    box-sizing: border-box;
+    width: 100%;
     padding: 4px;
-    font-family: Consolas, monospace;
-    background: #68c;
-    color: white;
     font-size: 12px;
-    line-height: 130%;
     min-height: 200px;
+    padding-top: 12px;
+    padding-bottom: 12px;
   }
   
   input[type=submit] {
-    border: 1px solid #369;
     padding: 4px;
     color: black;
-    background: #6c8;
     min-width: 80px;
     cursor: pointer;
+    margin-top: -1px;
   }
 
   input[type=submit]:hover {
@@ -102,3 +123,4 @@ root.require = nil
   }
 
 </style>
+    </article></body></html>
