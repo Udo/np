fb2360f319a4577f0f3a9167beb3783ba2daab04
Udo Schroeter <udo.schroeter@gmail.com>
2014-09-15 23:41:38 +0200

    bug in package loader

--- src/loadlib.c
+++ src/loadlib.c
@@ -573,7 +573,7 @@ static void modinit (lua_State *L, const char *modname) {
   lua_setfield(L, -2, "_M");  /* module._M = module */
   lua_pushstring(L, modname);
   lua_setfield(L, -2, "_NAME");
-  dot = strrchr(modname, '.');  /* look for last dot in module name */
+  dot = NULL;//strrchr(modname, '.');  /* look for last dot in module name */
   if (dot == NULL) dot = modname;
   else dot++;
   /* set _PACKAGE as package name (full module name minus last part) */
