f26db79f48416ce86d213ce66bfd8b578a6cae3d
udo <udo@openfu.com>
2020-09-27 18:24:07 +0200

    AST

--- src/cli.cpp
+++ src/cli.cpp
@@ -7,12 +7,12 @@ string src;
 
 int main(int argc, char** argv) 
 {
-	if(argc >= 1)
+	if(argc >= 2)
 	{
 		src = read_text_file(argv[1]);
 		auto token_list = tokenize(src);
-		token_list->print(true);
-		printf("\n");
+		//token_list->print(true);
+		//printf("\n");
 		auto ast = parse(token_list);
 		ast->print(true);
 	}
--- src/language/parser.h
+++ src/language/parser.h
@@ -13,7 +13,7 @@ struct ASTNode
 			printf("%s\u001b[32m%i \u001b[34m%i:%i \u001b[33m%s\u001b[0m\n", level.c_str(),
 				this->token->type, this->token->col, this->token->line, this->token->text.c_str());
 		else
-			printf("%s(empty)", level.c_str());
+			printf("%s(empty)\n", level.c_str());
 		if(this->child) this->child->print(true, level+"  ");
 		if(all && this->next) this->next->print(true, level);
 	}
@@ -21,12 +21,17 @@ struct ASTNode
 
 ASTNode* parse(Token* token_list)
 {
-	ASTNode* ast = new ASTNode();
+	ASTNode* ast_root = new ASTNode();
+	ASTNode* ast_prev = ast_root;
 	Token* token = token_list;
 	while(token)
 	{
+		ASTNode* n = new ASTNode();
+		n->token = token;
 		
 		token = token->next;
+		ast_prev->next = n;
+		ast_prev = n;
 	}
-	return(ast);
+	return(ast_root);
 }
